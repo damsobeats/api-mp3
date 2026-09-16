6b5630ec551ddc3006f2d94339f8eb72005605ec
damsobeats <123994438+damsobeats@users.noreply.github.com>
2026-09-16 17:11:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def download_audio():
     
     options = {
         'format': 'bestaudio/best',
+        'cookiefile': 'cookies.txt',  # <-- Ligne à ajouter ici
         'extractor_args': {'youtube': {'player_client': ['android']}},
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -25,6 +26,8 @@ def download_audio():
         }],
         'outtmpl': filename.replace('.mp3', ''),
     }
+        'outtmpl': filename.replace('.mp3', ''),
+    } 
 
     try:
         with yt_dlp.YoutubeDL(options) as ydl:
